8d4b8758a4eb44877e37222349981d946730cde5
Lisa Wild <lisakwild@gmail.com>
2024-03-18 13:25:17 -0700

    Create a renderTweets function that will cycle through an array of tweets and add them to the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,40 @@
  */
 
 $(document).ready(function(){
+
+  // Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+ 
+  const renderTweets = function(tweets) {
+    for (let tweetData of tweets) {
+      let $tweet = createTweetElement(tweetData);
+      $('.posted-tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    }
+  }
+
   const createTweetElement = function(tweetData) {
     const $tweet = $(`<article>
     <header class="tweet">
@@ -28,29 +62,12 @@ $(document).ready(function(){
   </article>`);
   
   return $tweet; 
-  }
+}
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
-  
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log("this is the tweet HTML-----------", $tweet); // to see what it looks like
-  $('.posted-tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(data);
 
 
-})
+});
 
 
 
