cdd23557febc9a91b52a117851de915bb1ac6195
Lisa Wild <lisakwild@gmail.com>
2024-03-19 14:50:44 -0700

    Create an ajax GET req to populate the page with new tweets; refractor code significantly with a mentor; correct duplicate tweets bug with emptying the .posted-tweets section element before loadTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,87 +4,91 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function(){
-
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
- 
-  const renderTweets = function(tweets) {
-    for (let tweetData of tweets) {
-      let $tweet = createTweetElement(tweetData);
-      $('.posted-tweets').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-    }
-  }
-
-  const createTweetElement = function(tweetData) {
-    const $tweet = $(`<article>
-    <header class="tweet">
-      <span>
-        <img class="avatar" src="${tweetData.user.avatars}">
-        <h3>${tweetData.user.name}</h3>
-      </span>
-      <h4>${tweetData.user.handle}</h4>
-    </header>
-    <section class="tweet-content">
-      <p>${tweetData.content.text}</p>
-    </section>
-    <footer class="tweet">
-      <p>${tweetData.created_at}</p>
-      <div>
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-repeat"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article>`);
-  
-  return $tweet; 
-  }
-
-  renderTweets(data);
-  
+$(document).ready(function () {
   // submit button action override - send data to server
-  $("form").on("submit", function(event) {
-    event.preventDefault();
-    let formData = $(this).serialize();
-    
-    $.ajax({
-      type: "POST",
-      url: "/tweets",
-      data: formData,
-      success: function(response) {
-        console.log("Data submitted successfully)");
-      },
-      error: function(error) {
-        console.log("Error submitting data:", error);
-      }
-    })
-  })
+  $("form").on("submit", submitTweet)
+
+  loadTweets();
 });
 
+const submitTweet = function (event) {
+  event.preventDefault();
+  let formData = $(this).serialize();
+  $.post("/tweets", formData)
+    .then(() => {
+      $('.posted-tweets').empty();
+      loadTweets();
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+}
 
+const loadTweets = function () {
+  $.get("/tweets")
+    .then(response => {
+      renderTweets(response);
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+}
 
+const renderTweets = function (tweets) {
+  for (let tweetData of tweets) {
+    let $tweet = createTweetElement(tweetData);
+    $('.posted-tweets').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  }
+}
 
+const createTweetElement = function (tweetData) {
+  const $tweet = $(`<article>
+            <header class="tweet">
+              <span>
+                <img class="avatar" src="${tweetData.user.avatars}">
+                <h3>${tweetData.user.name}</h3>
+              </span>
+              <h4>${tweetData.user.handle}</h4>
+            </header>
+            <section class="tweet-content">
+              <p>${tweetData.content.text}</p>
+            </section>
+            <footer class="tweet">
+              <p>${tweetData.created_at}</p>
+              <div>
+                <i class="fa-solid fa-flag"></i>
+                <i class="fa-solid fa-repeat"></i>
+                <i class="fa-solid fa-heart"></i>
+              </div>
+            </footer>
+          </article>`);
+
+  return $tweet;
+}
+
+//Jurassic ajax syntax
+  // $.ajax({
+  //   type: "POST",
+  //   url: "/tweets",
+  //   data: formData,
+  //   success: function(response) {
+  //     console.log("Data submitted successfully");
+  //     // loadTweets();
+  //   },
+  //   error: function(error) {
+  //     console.log("Error submitting data:", error);
+  //   }
+  // })
+
+  
+  // $.ajax({
+  //   type: "GET",
+  //   url: "/tweets",
+  //   success: function(response) {
+  //     console.log("Data gathered successfully");
+  //     renderTweets(response);
+  //   },
+  //   error: function(error) {
+  //     console.log("Error gathering data:", error);
+  //   }
+  // })
