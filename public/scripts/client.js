895297c45bac99b9742fddfa71bf5b34c88f10e8
Lisa Wild <lisakwild@gmail.com>
2024-03-28 09:19:08 -0700

    Create HTML article with jQuery (eliminate need for escaping XSS), refactor and lint code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,22 +4,24 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function () {
+$(document).ready(() => {
   // submit button action override - send data to server
-  $("form").on("submit", submitTweet)
+  $("form").on("submit", submitTweet);
 
   loadTweets();
 });
 
-const submitTweet = function (event) {
+
+//take the functions and put them in a helper.js file in scripts (link it in the html)
+const submitTweet = function(event) {
   event.preventDefault();
-  let formData = $(this).serialize();
+  let formData = $(this).serialize();              //the server is configured to receive data as a query string
   let $text = $('#tweet-text').val();
 
   if ($text === "") {
     $('.empty-tweet-err').slideDown("slow");
     $('.long-tweet-err').hide();
-  } else if($text.length > 140) {
+  } else if ($text.length > 140) {
     $('.long-tweet-err').slideDown();
     $('.empty-tweet-err').hide();
   } else {
@@ -30,84 +32,74 @@ const submitTweet = function (event) {
         $('#tweet-text').val("");
         $('.empty-tweet-err').slideUp("slow");
         $('.long-tweet-err').slideUp("slow");
+        $('.counter').text(140);
       })
       .catch((err) => {
         console.log(err);
       });
-    };
+  }
 };
 
-const loadTweets = function () {
+const loadTweets = function() {
   $.get("/tweets")
     .then(response => {
       renderTweets(response);
     })
     .catch(err => {
-      console.log(err.message)
-    })
-}
+      console.log(err.message);
+    });
+};
 
-const renderTweets = function (tweets) {
+const renderTweets = function(tweets) {
   for (let tweetData of tweets) {
     let $tweet = createTweetElement(tweetData);
     $('.posted-tweets').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
   }
-}
+};
 
-const escape = function(str) {
-  let section = document.createElement("section");
-  section.appendChild(document.createTextNode(str));
-  return section.innerHTML;
-}
 
-const createTweetElement = function (tweetData) {
-  const $tweet = $(`<article>
-            <header class="tweet">
-              <span>
-                <img class="avatar" src="${tweetData.user.avatars}">
-                <h3>${tweetData.user.name}</h3>
-              </span>
-              <h4>${tweetData.user.handle}</h4>
-            </header>
-            <section class="tweet-content">
-              <p>${escape(tweetData.content.text)}</p>
-            </section>
-            <footer class="tweet">
-              <p>${timeago.format(tweetData.created_at)}</p>
-              <div>
-                <i class="fa-solid fa-flag"></i>
-                <i class="fa-solid fa-repeat"></i>
-                <i class="fa-solid fa-heart"></i>
-              </div>
-            </footer>
-          </article>`);
+//using jQuery to create new HTML elements would remove the need to escape the XSS (.text)
+const createTweetElement = function(tweetData) {
+  const $tweet = $("<article>");
+  const $tweetHeader = $("<header>");
+  const $tweetSpan = $("<span>");
+  const $tweetUserAvatar = $("<img>");
+  const $tweetAuthor = $("<h3>");
+  const $tweetHandle = $("<h4>");
+  const $tweetContentContainer = $("<section>");
+  const $tweetContent = $("<p>");
+  const $tweetFooter = $("<footer>");
+  const $tweetPostTime = $("<p>");
+  const $tweetIconDiv = $("<div>");
+  const $tweetIconOne = $("<i>");
+  const $tweetIconTwo = $("<i>");
+  const $tweetIconThree = $("<i>");
+
+  $tweetHeader.attr("class", "tweet");
+  $tweetUserAvatar.attr({"src": `${tweetData.user.avatars}`, "class": "avatar"});
+  $tweetAuthor.text(`${tweetData.user.name}`);
+  $tweetHandle.text(`${tweetData.user.handle}`);
+  $tweetContent.attr("class", "tweet-content");
+  $tweetContent.text(`${tweetData.content.text}`);
+  $tweetFooter.attr("class", "tweet")
+  $tweetPostTime.text(`${timeago.format(tweetData.created_at)}`);
+  $tweetIconOne.attr("class", "fa-solid fa-flag")
+  $tweetIconTwo.attr("class", "fa-solid fa-repeat")
+  $tweetIconThree.attr("class", "fa-solid fa-heart")
+
+  $tweet.append($tweetHeader);
+  $tweetHeader.append($tweetSpan);
+  $tweetSpan.append($tweetUserAvatar);
+  $tweetSpan.append($tweetAuthor);
+  $tweetHeader.append($tweetHandle);
+  $tweet.append($tweetContentContainer);
+  $tweetContentContainer.append($tweetContent);
+  $tweet.append($tweetFooter);
+  $tweetFooter.append($tweetPostTime);
+  $tweetFooter.append($tweetIconDiv);
+  $tweetIconDiv.append($tweetIconOne);
+  $tweetIconDiv.append($tweetIconTwo);
+  $tweetIconDiv.append($tweetIconThree);
 
   return $tweet;
-}
-
-//Jurassic ajax syntax
-  // $.ajax({
-  //   type: "POST",
-  //   url: "/tweets",
-  //   data: formData,
-  //   success: function(response) {
-  //     console.log("Data submitted successfully");
-  //     // loadTweets();
-  //   },
-  //   error: function(error) {
-  //     console.log("Error submitting data:", error);
-  //   }
-  // })
-
-  
-  // $.ajax({
-  //   type: "GET",
-  //   url: "/tweets",
-  //   success: function(response) {
-  //     console.log("Data gathered successfully");
-  //     renderTweets(response);
-  //   },
-  //   error: function(error) {
-  //     console.log("Error gathering data:", error);
-  //   }
-  // })
+};
